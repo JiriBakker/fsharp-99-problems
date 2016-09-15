5ea1524c56b52c66baf1f6f164df5e62c8af85a3
Jiri Bakker <jiri.bakker@infi.nl>
2016-09-15 11:31:04 +0200

    Implemented problem 28b

---
+++ FSharp.NinetyNineProblems.Tests/28b.LengthFrequencySorterTests.fs
@@ -0,0 +1,35 @@
+﻿namespace FSharp.NinetyNineProblems.Tests
+
+open NUnit.Framework
+open FsUnit
+
+open FSharp.NinetyNineProblems
+
+// https://johncrane.gitbooks.io/ninety-nine-elm-problems/content/p/p28b.html
+
+[<TestFixture>]
+type LengthFrequencySorterTests() = 
+
+    [<Test>]
+    member this.sort_by_length_frequency_on_regular_list_of_lists_returns_sorted_output() = 
+        let testList =  [[1; 2; 3]; [6; 7; 8]; [0]; [2; 3; 5]]
+        LengthFrequencySorter.sortByLengthFrequency testList              
+        |> should equal [1; 3; 3; 3]
+
+    [<Test>]
+    member this.sort_by_length_frequency_on_list_of_lists_also_containing_an_empty_list_returns_sorted_output() = 
+        let testList = [[1]; [2]; [3]; [6; 7; 8]; [2; 34; 5]; []]
+        LengthFrequencySorter.sortByLengthFrequency testList              
+        |> should equal [0; 3; 3; 1; 1; 1]
+
+    [<Test>]
+    member this.sort_by_length_frequency_on_list_of_list_containing_very_long_list_returns_sorted_output() = 
+        let testList = [[1]; [2]; [3]; [6]; [2]; [1..100000]]
+        LengthFrequencySorter.sortByLengthFrequency testList              
+        |> should equal [100000; 1; 1; 1; 1; 1]
+
+    [<Test>]
+    member this.sort_by_length_frequency_on_list_containing_only_an_empty_list_returns_list_with_only_0() = 
+        let testList = [[]]
+        LengthFrequencySorter.sortByLengthFrequency testList              
+        |> should equal [0] 
---
+++ FSharp.NinetyNineProblems/28b.LengthFrequencySorter.fs
@@ -0,0 +1,11 @@
+﻿namespace FSharp.NinetyNineProblems
+
+// https://johncrane.gitbooks.io/ninety-nine-elm-problems/content/p/p28b.html
+
+module LengthFrequencySorter =    
+
+    let sortByLengthFrequency (input:'a list list) =
+        input
+        |> List.groupBy (fun sublist -> sublist.Length)
+        |> List.sortBy  (fun (key, sublists) -> sublists.Length)
+        |> List.collect (fun (key, sublists) -> List.replicate sublists.Length key)
